cf8ef23cad632044f9f8955d08894e2ed9c43c3c
xiongli <1428693744@qq.com>
2025-05-17 21:26:00 +0800

    push v1 feature

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,4 +7,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void v1() {
+        System.out.println("this is v1 feature.");
+    }
 }
