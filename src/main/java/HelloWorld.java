06c809a1e6f3d0df227de9e6f73559f58c8f537e
sonic-cmyk <1428693744@qq.com>
2025-06-15 14:06:52 +0800

    Get handlingTimes

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Get handlongTimes");
     }
 
     public void v1() {
