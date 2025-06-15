488d5ffa3d77e66d71d7ee961fc6ced9e90574f2
xiongli <1428693744@qq.com>
2025-06-15 14:35:38 +0800

    get shippingServices

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
         System.out.println("Hello World!");
         System.out.println("Get handlingTimes");
         System.out.println("Get shippingLocations");
+        System.out.println("Get shippingServices");
     }
 
     public void v1() {
